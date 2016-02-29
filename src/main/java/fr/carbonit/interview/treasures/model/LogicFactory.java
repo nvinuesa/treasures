57a7fa8239e76442adbb525dfcb96e267ade80b4
nico <nico@example.c>
2016-02-29 22:15:16 +0100

    corrected code quality errors

--- src/main/java/fr/carbonit/interview/treasures/model/LogicFactory.java
+++ src/main/java/fr/carbonit/interview/treasures/model/LogicFactory.java
@@ -8,35 +8,35 @@ public class LogicFactory {
     /*
     Method returns a new point.
      */
-    Point createPosition(int x, int y) {
+    public Point createPosition(int x, int y) {
         return new Point(x, y);
     }
 
     /*
     Method returns a new treasure.
      */
-    Treasure createTreasure(Point position, int quantity) {
+    public Treasure createTreasure(Point position, int quantity) {
         return new Treasure(position, quantity);
     }
 
     /*
     Method returns a new mountain.
      */
-    Mountain createMountain(Point position) {
+    public Mountain createMountain(Point position) {
         return new Mountain(position);
     }
 
     /*
     Method returns a new map.
      */
-    Map createMap(LogicFactory factory) {
+    public Map createMap(LogicFactory factory) {
         return new Map(factory);
     }
 
     /*
     Method returns a new adventurer.
      */
-    Adventurer createAdventurer(LogicFactory factory, String line, Map map) {
+    public Adventurer createAdventurer(LogicFactory factory, String line, Map map) {
         Adventurer adventurer = null;
         try {
             adventurer = new Adventurer(factory, line, map);
